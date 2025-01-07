a40170ac4c6e974c67848800f0babdc90f7823ff
isha chukekar <151957140+ichukekar@users.noreply.github.com>
2025-01-08 00:12:33 +0530

    integrated backend api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-export const backendBaseURL = "http://localhost:8080";
-// export const backendBaseURL = "https://kanban-board-uqm8.onrender.com";
+//export const backendBaseURL = "http://localhost:8080";
+export const backendBaseURL = "https://kanban-board-simple.onrender.com";
